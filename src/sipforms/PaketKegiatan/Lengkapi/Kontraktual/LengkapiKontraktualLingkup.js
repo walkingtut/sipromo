a73b7d39217692b61dd6c6a494b33776153e34f4
Antonius Darma Setiawan <a.darma.setiawan@MacBooks-MacBook-Air.local>
2017-08-25 10:06:27 +0700

    Add popup dialog on Lengkapi Kontraktual

--- src/sipforms/PaketKegiatan/Lengkapi/Kontraktual/LengkapiKontraktualLingkup.js
+++ src/sipforms/PaketKegiatan/Lengkapi/Kontraktual/LengkapiKontraktualLingkup.js
@@ -7,7 +7,13 @@ import {
     SegmentedButton,
     Button,
     ToolTip,
-    SearchField
+    SearchField,
+    GridCell,
+    Container,
+    Dialog,
+    FormPanel,
+    FieldSet,
+    TextField
     } from '@extjs/ext-react';
 import { Template } from '@extjs/reactor';
 import modelLingkup from './Model/LengkapiKontraktualLingkupModel';
@@ -21,6 +27,11 @@ Ext.require([
 
 export default class LengkapiKontraktualLingkup extends Component {
 
+    state = {
+        showDialog: false,
+        judul: ""
+    }
+
     store = Ext.create('Ext.data.Store', {
         autoLoad: true,
         model,
@@ -31,118 +42,192 @@ export default class LengkapiKontraktualLingkup extends Component {
         },
     });
 
+    storeGrid = Ext.create('Ext.data.Store', {
+        autoLoad: true,
+        modelLingkup,
+        pageSize: 0,
+        proxy: {
+            type: 'ajax',
+            url: 'resources/data/CompanyData.json'
+        } 
+    });
+
+    onSearch = (grid, info) => {
+        this.setState({ showDialog: true });
+        this.setState({judul: info.record.data.kodepaket + ' - ' + info.record.data.namapaket});
+    }
+
     render() {
 
+        const {showDialog} = this.state;
+
         return (
-            <Grid
-            title="Daftar Paket Kegiatan Kontraktual"
-            store={this.store}
-            plugins={{
-                gridviewoptions: true,
-                gridsummaryrow: true
-            }}
-            signTpl={this.signTpl}
-            shadow
-            height="550"
-        >
-            <Toolbar docked="top">     
-                <div style={{ marginRight: '20px' }}>Cari:</div>
-                <SearchField 
-                    ui="faded"
-                    placeholder="Search"
-                />              
-                <ToolTip showOnTap title="Pengaturan Kolom" trackMouse width="200">
-                    Untuk mengatur kolom pada tabel Paket Kegiatan, tekan tombol mouse kiri pada header tabel selama beberapa saat
-                </ToolTip>
-            </Toolbar>
-            <Column 
-                text="<b>Kode</b>" 
-                dataIndex="kodepaket" 
-                width="140"
-                align="center"
-                summaryRenderer={this.summarizerecord} />
-            <Column 
-                text="<b>Tahun</b>" 
-                dataIndex="tahun" 
-                width="80"
-                align="left"
-                hidden />               
-            <Column 
-                text="<b>Nama Paket</b>" 
-                dataIndex="namapaket" 
-                width="200" 
-                align="left"/>
-            <Column 
-                text="<b>Satker</>" 
-                dataIndex="satker" 
-                width="100"
-                align="left"
-                hidden />
-            <Column 
-                text="<b>Nama PPK</b>" 
-                dataIndex="namappk" 
-                align="left"
-                width="150" />
-            <Column 
-                text="<b>Penanggung Jawab</b>" 
-                dataIndex="penanggungjawab" 
-                align="left"
-                width="150" 
-                hidden />
-            <Column 
-                text="<b>Nilai Paket</b>" 
-                dataIndex="nilaipaket" 
-                width="150" 
-                formatter='currency("Rp",0,false," ")' 
-                align="right" 
-                summary="sum" />
-            <Column 
-                text="<b>No. Kontrak</b>" 
-                dataIndex="nokontrak" 
-                width="150"
-                align="center"
-                hidden />
-            <Column 
-                text="<b>Tanggal Kontrak</b>" 
-                dataIndex="tglkontrak" 
-                width="100" 
-                align="center"
-                hidden />
-            <Column 
-                text="<b>Jenis Paket</b>" 
-                dataIndex="jenispaket" 
-                width="120"
-                align="center" />
-            <Column text="<b>Durasi Kegiatan</b>" align="center">
-                <Column
-                    text="<b>Durasi</b>"
-                    dataIndex="durasikegiatan" 
+            <Container>
+                <Grid
+                title="Daftar Paket Kegiatan Kontraktual - Lengkapi Data Ruang Lingkup"
+                store={this.store}
+                plugins={{
+                    gridviewoptions: true,
+                    gridsummaryrow: true
+                }}
+                signTpl={this.signTpl}
+                shadow
+                height="550"
+            >
+                <Toolbar docked="top">     
+                    <div style={{ marginRight: '20px' }}>Cari:</div>
+                    <SearchField 
+                        ui="faded"
+                        placeholder="Search"
+                    />              
+                    <ToolTip showOnTap title="Pengaturan Kolom" trackMouse width="200">
+                        Untuk mengatur kolom pada tabel Paket Kegiatan, tekan tombol mouse kiri pada header tabel selama beberapa saat
+                    </ToolTip>
+                </Toolbar>
+                <Column 
+                    text="<b>Kode</b>" 
+                    dataIndex="kodepaket" 
+                    width="140"
+                    align="center"
+                    summaryRenderer={this.summarizerecord} />
+                <Column 
+                    text="<b>Tahun</b>" 
+                    dataIndex="tahun" 
                     width="80"
+                    align="left"
+                    hidden />               
+                <Column 
+                    text="<b>Nama Paket</b>" 
+                    dataIndex="namapaket" 
+                    width="200" 
+                    align="left"/>
+                <Column 
+                    text="<b>Satker</>" 
+                    dataIndex="satker" 
+                    width="100"
+                    align="left"
+                    hidden />
+                <Column 
+                    text="<b>Nama PPK</b>" 
+                    dataIndex="namappk" 
+                    align="left"
+                    width="150" />
+                <Column 
+                    text="<b>Penanggung Jawab</b>" 
+                    dataIndex="penanggungjawab" 
+                    align="left"
+                    width="150" 
+                    hidden />
+                <Column 
+                    text="<b>Nilai Paket</b>" 
+                    dataIndex="nilaipaket" 
+                    width="150" 
+                    formatter='currency("Rp",0,false," ")' 
+                    align="right" 
+                    summary="sum" />
+                <Column 
+                    text="<b>No. Kontrak</b>" 
+                    dataIndex="nokontrak" 
+                    width="150"
+                    align="center"
+                    hidden />
+                <Column 
+                    text="<b>Tanggal Kontrak</b>" 
+                    dataIndex="tglkontrak" 
+                    width="100" 
+                    align="center"
+                    hidden />
+                <Column 
+                    text="<b>Jenis Paket</b>" 
+                    dataIndex="jenispaket" 
+                    width="120"
                     align="center" />
-                <Column
-                    text="<b>Satuan</b>"
-                    dataIndex="satuandurasi" 
-                    width="80"
-                    align="center" />    
-            </Column>
-            <Column 
-                text="<b>No. SPMK</b>"                  
-                dataIndex="nospmk"
-                width="150"
-                align="center" 
-                hidden />
-            <Column 
-                text="<b>Penyedia Jasa</b>" 
-                dataIndex="penyediajasa" 
-                width="150"
-                align="center" 
-                hidden />
-            <Column 
-                text="<b>Tanggal Penyelesaian</b>" 
-                dataIndex="tanggalpenyelesaian" 
-                width="150"
-                align="center" />           
-        </Grid>
+                <Column text="<b>Durasi Kegiatan</b>" align="center">
+                    <Column
+                        text="<b>Durasi</b>"
+                        dataIndex="durasikegiatan" 
+                        width="80"
+                        align="center" />
+                    <Column
+                        text="<b>Satuan</b>"
+                        dataIndex="satuandurasi" 
+                        width="80"
+                        align="center" />    
+                </Column>
+                <Column 
+                    text="<b>No. SPMK</b>"                  
+                    dataIndex="nospmk"
+                    width="150"
+                    align="center" 
+                    hidden />
+                <Column 
+                    text="<b>Penyedia Jasa</b>" 
+                    dataIndex="penyediajasa" 
+                    width="150"
+                    align="center" 
+                    hidden />
+                <Column 
+                    text="<b>Tanggal Penyelesaian</b>" 
+                    dataIndex="tanggalpenyelesaian" 
+                    width="150"
+                    align="center" />     
+                <Column 
+                    text="Actions" 
+                    width="80" 
+                >
+                    <GridCell 
+                        tools={{
+                            search: {
+                                handler: this.onSearch
+                            }
+                        }}
+                    />
+                </Column>                            
+            </Grid>
+
+            <Dialog 
+                displayed={showDialog}
+                title={this.state.judul}
+                closable
+                maximizable
+                closeAction="hide"
+                maskTapHandler={this.onCancel}
+                bodyPadding="20"
+                width="1200"
+                defaultFocus="#ok"
+                onHide={() => this.setState({ showDialog: false })}
+            >
+                 <Container 
+                        autoSize 
+                        defaults={{ flex: 1, bodyPadding: 10, shadow: true, margin: 10 }} 
+                        layout={{ type: Ext.os.is.Phone ? 'vbox' : 'hbox', pack: 'center', align: 'stretch' }}
+                        flex={1}
+                        height="400"
+                    > 
+                    <Grid title="Stock Prices" store={this.storeGrid} shadow grouped flex="6">
+                        <Column text="Company" dataIndex="name" width="150"/>
+                        <Column text="Price" width="85" dataIndex="price" formatter='usMoney'/>
+                        <Column text="Change" width="100" dataIndex="priceChange"/>
+                        <Column text="% Change" dataIndex="priceChangePct" />
+                    </Grid>
+
+                    <FormPanel flex="4" height="300">
+                        <FieldSet title="<h4>Input/Edit Master Data Direktorat</h4>">
+                            <TextField label="Company"/>
+                            <TextField label="Price"/>
+                            <TextField label="Price Change"/>
+                        </FieldSet>
+                        <Toolbar shadow={false} docked="bottom" layout={{ type: 'hbox', pack: 'right' }}>
+                            <Button text="Batal" />
+                            <Button text="Simpan" />
+                        </Toolbar>
+                    </FormPanel>
+                </Container> 
+                    <Button text="Cancel" handler={this.onCancel}/>
+                    <Button itemId="ok" text="OK" handler={this.onOk}/>
+            </Dialog>
+
+        </Container>    
         )
     }
     
